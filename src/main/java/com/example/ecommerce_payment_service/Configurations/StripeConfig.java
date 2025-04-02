0bca705cbed13ee705dccc5a77438df6775f506f
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-04-02 23:47:09 +0200

    feat(stripe): Create Stripe configuration bean - Centralized Stripe API key setup for secure usage across the app

---
+++ src/main/java/com/example/ecommerce_payment_service/Configurations/StripeConfig.java
@@ -0,0 +1,18 @@
+package com.example.ecommerce_payment_service.Configurations;
+
+import com.stripe.Stripe;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StripeConfig {
+
+    @Value("${stripe.secret.key}")
+    private String secretKey;
+
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = secretKey;
+    }
+}
